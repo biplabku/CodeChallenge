fb32eb0caf37db93e6f0884dbb38bd2d04914e10
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-10 07:08:13 -0600

    Feat - Refactor. Completed the basic code for count frequencies

--- src/wordFrequencies.java
+++ src/wordFrequencies.java
@@ -2,7 +2,21 @@ public class wordFrequencies {
     // design a method to find the frequencies of occurences of any giiven word in a book.
     // What if we were running this algorithm mutiple times.
 
-    public static void main(String[] args) {
+    public int countFrequencies(String[] word, String comp) {
+        int count = 0;
+        for(int i = 0 ; i < word.length; i++) {
+            if(word[i].equals(comp)) {
+                count++;
+            }
+        }
+        return count;
+    }
 
+    public static void main(String[] args) {
+        wordFrequencies ws = new wordFrequencies();
+        String str = "My name is biplab is name yellow b";
+        String[] words = str.split(" ");
+        String s = "is";
+        System.out.println(ws.countFrequencies(words, s) );
     }
 }
