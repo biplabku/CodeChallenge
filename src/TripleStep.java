8b1cc667ea2d1e67bfb968dbcf49fc6a65455bf3
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-13 23:43:12 -0600

    Feat - New code

--- src/TripleStep.java
+++ src/TripleStep.java
@@ -11,8 +11,21 @@ public class TripleStep {
         }
     }
 
+    public static int countTheStepsMethod2(int number, int[] memo) {
+        if(number < 0) {
+            return 0;
+        }else if(number == 0) {
+            return 1;
+        }else if(memo[number] > -1) {
+            return memo[number];
+        }else {
+            memo[number] = countTheStepsMethod2(number - 1, memo) + countTheStepsMethod2(number - 2, memo) + countTheStepsMethod2(number - 3, memo);
+        }
+        return memo[number];
+    }
+
     public static void main(String[] args) {
-        int number  = 2;
+        int number  = 1;
         System.out.println(countTheSteps(number));
     }
 }
---
+++ src/longestSubstring.java
@@ -0,0 +1,8 @@
+public class longestSubstring {
+
+    // find the longest subtring without any repeating characters
+
+    public static void main(String[] args) {
+
+    }
+}
