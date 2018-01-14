19ef51998dc086b5a820972013e1f628eb8e8675
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-13 23:51:07 -0600

    Feat - Adding subarray code

---
+++ src/subArraySum.java
@@ -0,0 +1,18 @@
+public class subArraySum {
+
+
+    // Given a list of  numbers, determine if a contiguous elements of the list sum to an input number
+
+    public String findList(int[] array, int target) {
+        
+    }
+
+    public static void main(String[] args) {
+        subArraySum ss = new subArraySum();
+        int[] array = new int[] {5,2,3,4, 9};
+        int target = 5;
+
+        System.out.println(ss.findList(array, target));
+
+    }
+}
