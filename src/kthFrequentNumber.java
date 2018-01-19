6b814b5cbd2e43858f14b0775021fe1311b57b07
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-19 05:34:19 -0600

    Feat - new feature. Adding Kth most frequent question

---
+++ src/kthFrequentNumber.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class kthFrequentNumber {
+
+
+    // one way is to sort the elements and then second most repeated values
+
+    public int findKth(int[] array, int k) {
+
+
+
+    }
+
+    public static void main(String[] args) {
+        kthFrequentNumber kk = new kthFrequentNumber();
+        int[] array = new int[]{1,2,3,2,1,2,2,2,3};
+        System.out.println(kk.findKth(array, ));
+    }
+}
