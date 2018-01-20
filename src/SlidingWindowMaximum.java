c762d53e65e6da2fe50a2011d33f56d5419a80f8
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-20 09:42:31 -0600

    Feat - Bug fix. Completed the code for sliding window

--- src/SlidingWindowMaximum.java
+++ src/SlidingWindowMaximum.java
@@ -5,17 +5,31 @@ public class SlidingWindowMaximum {
     // array to the very right. You can only see the k numbers in the window. Each time the sliding
     // window moves right by one position.
 
-    public ArrayList<Integer> findMax(int[] array, int num) {
+    public ArrayList<Integer> findMaxSlidingWindow(int[] array, int num) {
+        if(num > array.length ) {
+            return null;
+        }
         ArrayList<Integer> list = new ArrayList<>();
-
-
+        for(int i = 0; i < (array.length - num + 1); i++) {
+            list.add(findMax(array, i, (i + num)));
+        }
         return list;
+    }
+
+    public int findMax(int[] array, int index, int j) {
+        int max = array[index];
+        for(int i = index; i < j; i++) {
+            if(max < array[i]) {
+                max = array[i];
+            }
+        }
+        return max;
     }
 
     public static void main(String[] args) {
         SlidingWindowMaximum ss = new SlidingWindowMaximum();
         int[] array = new int[] {1,3,-1,-3,5,3,6,7};
         int pos = 3;
-        System.out.println(ss.findMax(array, pos));
+        System.out.println(ss.findMaxSlidingWindow(array, pos));
     }
 }
