2e3d2c66bda74e72b18df3535dcbbdac46b07ad4
Biplab Kumar Das <dasbiplabtu@gmail.com>
2018-01-21 11:31:05 -0600

    Feat - New Feature. Adding Knapsack problem

---
+++ src/knapsackProblem.java
@@ -0,0 +1,9 @@
+public class knapsackProblem {
+
+    // the knapsack problem is as follows: given a set of integers S= {1,2,5,9,10}, and a given
+    // target number 22, find a subset of S which adds up exactly to T.
+    // find counterexamples to each of the following algorithms for the knapsack problem. That is give an S
+    // and T such that the subset is selected using the algorithm does not leave the knapsack completly full, even
+    // though such a soltuion exists
+    
+}
